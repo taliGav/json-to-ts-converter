db19f5534d8e5df6da1e234db5eca508aa795719
Tali <taligavrielov@gmail.com>
2022-10-24 17:49:18 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppMain from './App-main';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <AppMain />
+
     </div>
   );
 }
