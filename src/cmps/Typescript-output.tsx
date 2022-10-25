8c1f739a98b7dbcee7a9cbffd6ba5964dd00e1ed
Tali <taligavrielov@gmail.com>
2022-10-25 13:12:22 +0300

    add output component

---
+++ src/cmps/Typescript-output.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+function TypeScriptOutput() {
+    return (
+        <div className="Ts-output-container">
+
+
+            ts   output box
+
+        </div>
+    );
+}
+
+export default TypeScriptOutput;
