225ac536989800fab1dc36a6d5e56050ea18d886
Tali <taligavrielov@gmail.com>
2022-10-25 13:12:41 +0300

    add input cmp

---
+++ src/cmps/Json-input.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+
+
+function JsonInput() {
+    return (
+        <div className="Json-input-container">
+            JSON input main container
+
+            <form action="" method="post">
+                JSON input form container
+
+
+                {/* <textarea name="Json-input-text" id="" cols="30" rows="10"></textarea> */}
+
+                <br />
+                <input type="submit" value="Convert to TS" />
+            </form>
+
+        </div>
+    );
+}
+
+export default JsonInput;
