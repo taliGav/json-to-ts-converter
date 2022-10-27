07829705590d5db03f7e33a58ad0a03a0e78e39c
Tali <taligavrielov@gmail.com>
2022-10-27 17:49:15 +0300

    update import filename

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import JsonInput from './cmps/Json-input';
-import TypeScriptOutput from './cmps/Typescript-output';
+import JsonInput from './cmps/JsonInput';
+import TypeScriptOutput from './cmps/TypescriptOutput';
 
 
 function AppMain() {
   return (
     <div className="App-main"
       style={{
+        backgroundColor: "yellow",
+        minHeight: "90vh",
         display: "inline-flexbox",
         flexDirection: "column",
         justifyContent: "center"
