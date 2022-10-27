30da3b7727251f606ef634100663238163445fc1
Tali <taligavrielov@gmail.com>
2022-10-27 17:50:01 +0300

    update cmps filename to camel case

---
+++ src/cmps/TypescriptOutput.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+
+function TypeScriptOutput() {
+    return (
+        <div className="Ts-output-container"
+        style={{
+            display: "inline-flexbox",
+            backgroundColor: "green",
+            maxWidth: "40%"
+          }}>
+            ts output box
+        </div>
+    );
+}
+
+export default TypeScriptOutput;
