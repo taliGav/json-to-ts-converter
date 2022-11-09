93caa72663d2a080e56b6e40ec6fcb2790b018ff
Tali <taligavrielov@gmail.com>
2022-11-09 19:56:54 +0200

    Update to conditional cmp, add the convert function and props to tsOutput cmp.

--- src/cmps/TypescriptOutput.tsx
+++ src/cmps/TypescriptOutput.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 
+function TypeScriptOutput(props: { updatedJsonInput: string }) {
 
+    if (props.updatedJsonInput) {
 
+        return (
+            <div className="Ts-output-container"
+                style={{
+                    display: "inline-flexbox",
+                    backgroundColor: "#497174",
+                    maxWidth: "100vw"
+                }}>
+                <h1>TypeScript Output</h1>
+                {JSON.stringify(tsInterfaceOutput(props.updatedJsonInput))}
+            </div>
+        );
+    }
 
-
-function TypeScriptOutput() {
     return (
-        <div className="Ts-output-container"
-        style={{
-            display: "inline-flexbox",
-            backgroundColor: "green",
-            maxWidth: "40%"
-          }}>
-            ts output box
+        <div>
+            <h1>TypeScript Output</h1>
+            <h3>Please insert json</h3>
         </div>
-    );
+    )
 }
+
+
+function tsInterfaceOutput(x: string) {
+    const parsedInput = JSON.parse(x)
+    const valuesMappedToTypesEntries = Object.entries(parsedInput).map(([key, val]) => {
+        return [key, typeof val]
+    })
+    
+    // console.log("valuesMappedToTypesEntries:" , valuesMappedToTypesEntries)
+    // console.log("Object.fromEntries:" , Object.fromEntries(valuesMappedToTypesEntries))
+    // // return [new String(key), typeof val]
+    // console.log("JSON.stringify:" , JSON.stringify(Object.fromEntries(valuesMappedToTypesEntries)))
+
+    return Object.fromEntries(valuesMappedToTypesEntries)
+}
+
 
 export default TypeScriptOutput;
