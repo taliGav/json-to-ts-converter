0d29b801518fe795b60b351c69ad772bc6dc292b
Tali <taligavrielov@gmail.com>
2022-11-17 20:12:51 +0200

    add footer to app

---
+++ src/cmps/Footer.tsx
@@ -0,0 +1,21 @@
+function Footer() {
+
+    return (
+        <div className="Footer"
+            style={{
+                backgroundColor: "#DEF5E5",
+                height: "10vh", 
+                padding: "10px",
+                alignItems: "center",
+                display: "inline-flexbox",
+                flexDirection: "column",
+                justifyContent: "center"
+            }}
+        >
+            <h1>Footer</h1>
+        </div >
+    );
+
+}
+
+export default Footer;
