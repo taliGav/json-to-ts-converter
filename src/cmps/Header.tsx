ebeb78ed67db76e2afea1eb87c9da45338128e29
Tali <taligavrielov@gmail.com>
2022-11-17 20:13:01 +0200

    add header to app

---
+++ src/cmps/Header.tsx
@@ -0,0 +1,22 @@
+function Header() {
+
+
+    return (
+      <div className="Header"
+        style={{
+          backgroundColor: "#8EC3B0",
+          height: "10vh",
+          padding: "10px",
+          display: "inline-flexbox",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+      >
+          <h1>Header</h1>
+      </div >
+    );
+  
+  }
+  
+  export default Header;
+  
