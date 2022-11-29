377d7e1ed8fa28e7c78d7d560c5c03020793deec
Tali <taligavrielov@gmail.com>
2022-11-29 23:18:17 +0200

    change header style settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@chakra-ui/react';
+import { Box , Heading} from '@chakra-ui/react';
+import { ArrowRightIcon} from '@chakra-ui/icons';
 
 
 function Header() {
@@ -7,8 +8,11 @@ function Header() {
       bg="#8EC3B0"
       h="10vh"
       p={2.5}
+      textAlign={['left', 'left', 'center', 'center']}
+      fontSize={['md', 'md', 'lg', 'lg']}
+      minW="480px"
       >
-          <h1>Header</h1>
+          <Heading>JSON <ArrowRightIcon w={6} h={6} pb={1.5}/>  TS Interfaces Converter</Heading>
       </Box >
     );
   
