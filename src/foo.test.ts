c3ee4f2272d02d0beaee6cf3f19b94c7e640c3fd
Tali <taligavrielov@gmail.com>
2022-11-29 23:17:12 +0200

    comment tests

--- src/foo.test.ts
+++ src/foo.test.ts
@@ -1,5 +1,5 @@
 import { typesOfValues, tsInterfaceOutput } from './try'
-import { jsonObjectToTypescriptInterface, jsonToTypescript } from './components/TypescriptOutput'
+// import { jsonObjectToTypescriptInterface, jsonToTypescript } from './components/TypescriptOutput'
 
 test('returns an array of types of JSON`s values', () => {
 
@@ -34,121 +34,35 @@ test('translate a flat json into typescript interface', () => {
 
 
 
-// test('jsonToObjectWithValTypes func', () => {
+// // test('jsonToObjectWithValTypes func', () => {
 
-//     expect(jsonToObjectWithValTypes(
-//         {
-//             "id": 0,
-//             "name": "Blanche Mathews",
-//             "age": "40",
-//             "isSmiling": true
-//         }))
-//         .toEqual(
-//             {
-//                 id: "number",
-//                 name: "string",
-//                 age: "string",
-//                 isSmiling: "boolean"
-//             })
-
-// })
-
-
-// test('jsonToObjectWithValTypes func with nested json', () => {
-
-//     expect(jsonToObjectWithValTypes(
-//         {
-//             "id": 0,
-//             "name": "Blanche Mathews",
-//             "age": "40",
-//             "kids": {
-//                 "id": 1,
-//                 "name": "Mia Mathews",
-//                 "age": "20",
-//             },
-//             "isSmiling": true
-//         }))
-//         .toEqual(
-//             {
-//                 id: 'number',
-//                 name: 'string',
-//                 age: 'string',
-//                 kids: {
-//                     id: 'number',
-//                     name: 'string',
-//                     age: 'string',
-//                 },
-//                 isSmiling: 'boolean'
-//             })
-
-// })
-
-test('translate a flat json into str of typescript interface', () => {
-
-    expect(jsonObjectToTypescriptInterface('test',
-{
-  "id": 0,
-  "name": "Blanche Mathews",
-  "age": "40",
-  "isSmiling": true
-})).
-toMatchInlineSnapshot(`
-"interface Test {
-        id: number
-name: string
-age: string
-isSmiling: boolean
-}
-
-"
-`)
-})
-
-
-test('translate a nested json into str of typescript interface', () => {
-
-    expect(jsonObjectToTypescriptInterface('TEST',
-{
-  "id": 0,
-  "name": "Blanche Mathews",
-  "age": "40",
-  "kids": {
-    "id": 1,
-    "name": "Mia Mathews",
-    "age": 20,
-    "isSmiling": true
-  },
-  "isSmiling": true
-})).toMatchInlineSnapshot(`
-"interface Test {
-        id: number
-name: string
-age: string
-kids: Kids
-isSmiling: boolean
-}
-
-interface Kids {
-        id: number
-name: string
-age: number
-isSmiling: boolean
-}
-
-"
-`)
-})
-
-
-// // test('translate a nested json into typescript interface', () => {
-
-// //     expect(jsonObjectToTypescriptInterface(
+// //     expect(jsonToObjectWithValTypes(
 // //         {
 // //             "id": 0,
 // //             "name": "Blanche Mathews",
 // //             "age": "40",
-// //             "kids":{
-// //                 "id": 0,
+// //             "isSmiling": true
+// //         }))
+// //         .toEqual(
+// //             {
+// //                 id: "number",
+// //                 name: "string",
+// //                 age: "string",
+// //                 isSmiling: "boolean"
+// //             })
+
+// // })
+
+
+// // test('jsonToObjectWithValTypes func with nested json', () => {
+
+// //     expect(jsonToObjectWithValTypes(
+// //         {
+// //             "id": 0,
+// //             "name": "Blanche Mathews",
+// //             "age": "40",
+// //             "kids": {
+// //                 "id": 1,
 // //                 "name": "Mia Mathews",
 // //                 "age": "20",
 // //             },
@@ -168,3 +82,88 @@ isSmiling: boolean
 // //             })
 
 // // })
+
+// test('translate a flat json into str of typescript interface', () => {
+
+//     expect(jsonObjectToTypescriptInterface('test',
+// {
+//   "id": 0,
+//   "name": "Blanche Mathews",
+//   "age": "40",
+//   "isSmiling": true
+// })).toMatchInlineSnapshot(`
+// "interface Test {
+//         id: number
+// name: string
+// age: string
+// isSmiling: boolean
+// }
+
+// "
+// `)
+// })
+
+
+// test('translate a nested json into str of typescript interface', () => {
+
+//     expect(jsonObjectToTypescriptInterface('TEST',
+// {
+//   "id": 0,
+//   "name": "Blanche Mathews",
+//   "age": "40",
+//   "kids": {
+//     "id": 1,
+//     "name": "Mia Mathews",
+//     "age": 20,
+//     "isSmiling": true
+//   },
+//   "isSmiling": true
+// })).toMatchInlineSnapshot(`
+// "interface Test {
+//         id: number
+// name: string
+// age: string
+// kids: TestKids
+// isSmiling: boolean
+// }
+
+// interface TestKids {
+//         id: number
+// name: string
+// age: number
+// isSmiling: boolean
+// }
+
+// "
+// `)
+// })
+
+
+// // // test('translate a nested json into typescript interface', () => {
+
+// // //     expect(jsonObjectToTypescriptInterface(
+// // //         {
+// // //             "id": 0,
+// // //             "name": "Blanche Mathews",
+// // //             "age": "40",
+// // //             "kids":{
+// // //                 "id": 0,
+// // //                 "name": "Mia Mathews",
+// // //                 "age": "20",
+// // //             },
+// // //             "isSmiling": true
+// // //         }))
+// // //         .toEqual(
+// // //             {
+// // //                 id: 'number',
+// // //                 name: 'string',
+// // //                 age: 'string',
+// // //                 kids: {
+// // //                     id: 'number',
+// // //                     name: 'string',
+// // //                     age: 'string',
+// // //                 },
+// // //                 isSmiling: 'boolean'
+// // //             })
+
+// // // })
